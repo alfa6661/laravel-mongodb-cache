e24008015fd8e61955f576d2edec1d9b1d6fc5ff
mya-zaki <m90ch1@gmail.com>
2018-03-02 15:30:58 +0900

    upsert cache key

--- src/MongoStore.php
+++ src/MongoStore.php
@@ -19,6 +19,25 @@ class MongoStore extends DatabaseStore
     {
         return new TaggedCache($this, new TagSet($this, is_array($names) ? $names : func_get_args()));
     }
+    
+    /**
+     * Store an item in the cache for a given number of minutes.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @param  float|int  $minutes
+     * @return void
+     */
+    public function put($key, $value, $minutes)
+    {
+        $key = $this->prefix.$key;
+
+        $value = serialize($value);
+
+        $expiration = $this->getTime() + (int) ($minutes * 60);
+
+        $this->table()->where('key', $key)->update(compact('value', 'expiration'), ['upsert' => true]);
+    }
 
     /**
      * Increment or decrement an item in the cache.
